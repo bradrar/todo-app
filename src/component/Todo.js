2cd841b2d6ad17e331c8ed95545ce5f537caf714
bradrar <bradbaguyo@gmail.com>
2020-05-26 12:40:21 +0800

    remove setfinshed state that is not in use

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -3,7 +3,6 @@ import React, {useState} from 'react'
 export default function Todo() {
     const [list, setList] = useState([]);
     const [item, setItem] = useState("");
-    const [finishedItem, setFinishedItem] = useState("");
    
     function addTodo(e) {
         e.preventDefault();
@@ -16,8 +15,6 @@ export default function Todo() {
         e.preventDefault()
         setItem(e.target.value)
     }
-
-
 
     function handleFinished(props) {
         let newList = [...list] //new copy of list array
